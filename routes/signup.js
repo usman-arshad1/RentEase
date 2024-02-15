d4d3b58091afd56d6955166bafafbc03e78c717c
Noorarjun Sidhu <nsidhu73@my.bcit.ca>
2024-02-14 16:40:01 -0800

    Added basic server side input validation and client side feedback

--- routes/signup.js
+++ routes/signup.js
@@ -1,12 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('signup', { title: 'Signup - RentEase' });
+function validateInput(data) {
+  const { email, password, role } = data;
+  const resData = {};
+
+  if (!email) {
+    resData['emailInvalid'] = 'Please enter a valid email';
+  }
+
+  if (!password) {
+    resData['passwordInvalid'] = 'Please enter a valid password';
+  }
+
+  if (!role) {
+    resData['roleInvalid'] = 'Please enter a valid role';
+  }
+
+  return resData;
+}
+
+router.get('/', function (req, res, next) {
+  res.render('signup');
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
+  const resData = validateInput(req.body);
 
+  if (resData) {
+    res.status(400).render('signup', { resData });
+    return;
+  }
+  
+  res.status(304).render('signup');
 });
 
 module.exports = router;
