5a3164bbcec6484f6594215aca327ebfbd5e592c
krispyrendang <taswinder.s@gmail.com>
2024-02-27 16:38:51 -0800

    Added app.listen and port 8080 for local hosting on app.js

--- app.js
+++ app.js
@@ -1,48 +1,53 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const dotenv = require('dotenv');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const dotenv = require("dotenv");
+const port = process.env.PORT || 8080;
 
 dotenv.config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var signupRouter = require('./routes/signup');
-var loginRouter = require('./routes/login');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var signupRouter = require("./routes/signup");
+var loginRouter = require("./routes/login");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/signup', signupRouter);
-app.use('/login', loginRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/signup", signupRouter);
+app.use("/login", loginRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
+});
+
+app.listen(port, () => {
+	console.log("Node application listening on port " + port);
 });
 
 module.exports = app;
