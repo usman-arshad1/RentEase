676a36e59d5a0c718b077d9a5953b7c7f93f780f
Usman Arshad <uarshad2012@gmail.com>
2024-03-05 15:37:50 -0800

    updates schema using introspect

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'RentEase Test' });
 });
+
+const userData = {
+  first_name: 'John',
+  last_name: 'Doe',
+  password: 15, // You should hash the password before storing it
+  role: 2
+  // Other user properties as needed
+};
+
+async function createUser() {
+  try {
+    const createdUser = await prisma.user.create({
+      data: userData,
+    });
+
+    console.log('User created:', createdUser);
+  } catch (error) {
+    console.error('Error creating user:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+createUser();
 
 module.exports = router;
