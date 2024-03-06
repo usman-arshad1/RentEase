e600303422c940e781cead2b398f131e0af692a2
Noorarjun Sidhu <nsidhu73@my.bcit.ca>
2024-03-06 14:52:26 -0800

    Verify that a landlord can only view the page and landlords with those tenants can only remove them

--- routes/tenant_list.js
+++ routes/tenant_list.js
@@ -18,6 +18,10 @@ router.get("/", async function (req, res, next) {
 				return res.redirect('/login');
 			}
 		} else {
+			if (decoded.role === 2) {
+				return res.redirect('/');
+			}
+
 			try {
 				const properties = await prisma.properties.findMany({
 					where: {
@@ -44,12 +48,72 @@ router.get("/", async function (req, res, next) {
 				tenants.map(tenant => {
 					const property = properties.find(property => property.property_id === tenant.property_fk);
 					const combined = {...property, ...tenant};
+					combined.action = `landlord-tenant-list/remove/${combined.user_id}`;
 					results.push(combined);
 				});
 
 				resData['tenants'] = results;
 
-				res.render("tenant_list", { title: "List of Tenants - RentEase", resData });
+				return res.render("tenant_list", { title: "List of Tenants - RentEase", resData });
+			} catch (err) {
+				console.log(err);
+			} finally {
+				prisma.$disconnect();
+			}
+		}
+	});
+});
+
+router.get("/remove/:user_id", async function (req, res, next) {
+	const resData = {};
+	const existingToken = req.cookies.jwt;
+	const userId = parseInt(req.params.user_id, 10);
+
+	if (isNaN(userId)) {
+		return res.redirect('/landlord-tenant-list');
+	}
+
+	if (!existingToken) {
+		return res.redirect('/login');
+	}
+
+	jwt.verify(existingToken, process.env.JWT_SECRET, async (err, decoded) => {
+		if (err) {
+			if (err.name === 'TokenExpiredError') {
+				return res.redirect('/login');
+			}
+		} else {
+			if (decoded.role === 2) {
+				return res.redirect('/');
+			}
+
+			try {
+				const properties = await prisma.properties.findMany({
+					where: {
+						user_id: decoded.user_id
+					}
+				});
+
+				const user = await prisma.user.findUnique({
+					where: {
+						user_id: userId
+					}
+				})
+
+				const foundUser = properties.find(property => property.property_id == user.property_fk);
+
+				if (foundUser) {
+					await prisma.user.update({
+						where: {
+							user_id: user.user_id
+						},
+						data: {
+							property_fk: null
+						}
+					})
+				}
+
+				return res.redirect('landlord-tenant-list');
 			} catch (err) {
 				console.log(err);
 			} finally {
