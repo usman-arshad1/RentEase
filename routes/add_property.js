588fda59b6f0a1587b6866471e1af648f78b39be
syoo0295 <72421499+syoo0295@users.noreply.github.com>
2024-03-06 17:52:42 -0800

    Add user properties display table

--- routes/add_property.js
+++ routes/add_property.js
@@ -1,8 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 router.get('/', function(req, res, next) {
   res.render('add_property', { title: 'Landlord Dashboard - Add property' });
+});
+
+// Create a new property
+router.post('/', async (req, res) => {
+  let newProperty; // Declare newProperty outside try-catch block for broader scope
+  try {
+    // Get data from body
+    // The 'user_id' field should not be filled in by the user.
+    // Instead, it should be set on the server-side after verifying the user's identity.
+    // For now, user id is set to 5 which is "landlord test".
+    const {unit, street, city, province_state, country} = req.body;
+
+    // Create a new property
+    newProperty = await prisma.properties.create({
+      data: {
+        unit: unit,
+        street: street,
+        city: city,
+        province_state: province_state,
+        country: country,
+        user_id: 5
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("An error occurred while creating the property");
+  }
+
+  // Send response
+  return res.json(newProperty);
 });
 
 module.exports = router;
--- routes/property.js
+++ routes/property.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var router = express.Router();
+const { PrismaClient} = require("@prisma/client");
+const session = require("express-session");
+const prisma = new PrismaClient();
 
-router.get("/", function (req, res, next) {
-	res.render("property", { title: "Landlord Properties" });
+router.get("/", async function (req, res, next) {
+	// For now, user id is set to 5
+	// Implement function to get current session's user ID
+	const current_user_id = 5
+	const userProperties = await prisma.properties.findMany({
+		where: {
+			user_id: current_user_id
+		}
+	})
+	res.render("property", {
+		title: "Landlord Properties",
+		properties: userProperties
+	});
 });
 
 module.exports = router;
