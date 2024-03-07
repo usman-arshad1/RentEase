92a5577045bdb8b239282a7e27fa3155bd050b5d
Noorarjun Sidhu <nsidhu73@my.bcit.ca>
2024-03-06 21:58:12 -0800

    Send emails to others and property select will contain properties of a landlord that do not have a tenant

--- routes/tenant_list.js
+++ routes/tenant_list.js
@@ -1,8 +1,49 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
+function validateInput(email) {
+	const resData = {};
+
+	if (!email) {
+		resData['emailInvalid'] = 'Please enter a valid email';
+	}
+
+	return resData;
+}
+
+function sendEmail(email) {
+	let returnValue = 0;
+
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: process.env.EMAIL_USER,
+			pass: process.env.EMAIL_PASS
+		}
+	});
+
+	const mailOptions = {
+		from: process.env.EMAIL_USER,
+		to: email,
+		subject: 'Yoo',
+		text: 'Yoooooooo'
+	};
+
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			console.log(error);
+			returnValue = -1;
+		} else {
+			console.log('Email sent: ' + info.response);
+		}
+	});
+
+	return returnValue;
+}
 
 router.get("/", async function (req, res, next) {
 	const resData = {};
@@ -43,16 +84,25 @@ router.get("/", async function (req, res, next) {
 					}
 				});
 
-				const results = [];
+				const tenantResults = [];
 
-				tenants.map(tenant => {
+				tenants.forEach(tenant => {
 					const property = properties.find(property => property.property_id === tenant.property_fk);
-					const combined = {...property, ...tenant};
+					const combined = { ...property, ...tenant };
 					combined.action = `landlord-tenant-list/remove/${combined.user_id}`;
-					results.push(combined);
+					tenantResults.push(combined);
 				});
 
-				resData['tenants'] = results;
+				const propertyResults = await prisma.properties.findMany({
+					where: {
+						property_id: {
+							notIn: tenants.map(tenant => tenant.property_fk)
+						}
+					}
+				});
+
+				resData['tenants'] = tenantResults;
+				resData['properties'] = propertyResults;
 
 				return res.render("tenant_list", { title: "List of Tenants - RentEase", resData });
 			} catch (err) {
@@ -113,7 +163,7 @@ router.get("/remove/:user_id", async function (req, res, next) {
 					})
 				}
 
-				return res.redirect('landlord-tenant-list');
+				return res.redirect('/landlord-tenant-list');
 			} catch (err) {
 				console.log(err);
 			} finally {
@@ -121,6 +171,19 @@ router.get("/remove/:user_id", async function (req, res, next) {
 			}
 		}
 	});
+});
+
+router.post('/', async function (req, res, next) {
+	const { email } = req.body;
+	const resData = validateInput(email);
+
+	// if (Object.keys(resData).length > 0) {
+	// 	return res.status(400).render('tenant_list', { resData });
+	// }
+
+	if (sendEmail(email) === 0) {
+		return res.redirect('/landlord-tenant-list');
+	}
 });
 
 module.exports = router;
