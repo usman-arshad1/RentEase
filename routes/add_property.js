0a1782e830cba928d1050c797294ac53864f32b1
syoo0295 <72421499+syoo0295@users.noreply.github.com>
2024-03-06 21:37:46 -0800

    Implement authentication for property and add_property routes

--- routes/add_property.js
+++ routes/add_property.js
@@ -2,23 +2,43 @@ var express = require('express');
 var router = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const jwt = require('jsonwebtoken')
+function verifyLandlord(req, res, next) {
+  const token = req.cookies.jwt;
+  if (!token) {
+    return res.status(401).send("Access Denied / No token provided");
+  }
 
-router.get('/', function(req, res, next) {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.role !== 1) {
+      return res.status(403).send("Access Denied / Not a landlord");
+    }
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).send("Invalid token");
+  }
+}
+router.get('/', verifyLandlord, function(req, res, next) {
   res.render('add_property', { title: 'Landlord Dashboard - Add property' });
 });
 
 // Create a new property
-router.post('/', async (req, res) => {
-  let newProperty;
+router.post('/', verifyLandlord, async (req, res) => {
+  if (!req.user || !req.user.user_id) {
+    return res.status(400).send('User or user_id is not defined');
+  }
   try {
-    // Get data from body
-    // The 'user_id' field should not be filled in by the user.
-    // Instead, it should be set on the server-side after verifying the user's identity.
-    // For now, user id is set to 5 which is "landlord test".
+    let newProperty;
+    // Extract the current user's ID from the request
+    const currentUserId = req.user.user_id;
+
+    // parseInt unit number
     let unitString = req.body.unit;
     let unit = parseInt(unitString);
-    const { street, city, province_state, country} = req.body;
 
+    const { street, city, province_state, country} = req.body;
     // Create a new property
     newProperty = await prisma.properties.create({
       data: {
@@ -27,16 +47,16 @@ router.post('/', async (req, res) => {
         city: city,
         province_state: province_state,
         country: country,
-        user_id: 5
+        user_id: currentUserId
       },
     });
+
+    return res.redirect('/landlord-properties')
 
   } catch (error) {
     console.error(error);
     return res.status(500).send("An error occurred while creating the property");
   }
-
-  return res.redirect('/landlord-properties')
 });
 
 module.exports = router;
