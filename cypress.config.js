962132fa3f0c57b9e36444eb1b586947270d1eac
Usman Arshad <uarshad2012@gmail.com>
2024-03-08 05:31:21 -0800

    cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
