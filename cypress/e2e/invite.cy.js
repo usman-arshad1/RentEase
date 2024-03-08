181967cdf97156368e9b9ed5bd67e5c83f1ed112
Usman Arshad <uarshad2012@gmail.com>
2024-03-08 12:00:23 -0800

    invite automated testing

---
+++ cypress/e2e/invite.cy.js
@@ -0,0 +1,20 @@
+describe('announcement', () => {
+
+    it('delete tenant, invite', () => {
+        cy.visit('http://localhost:8080/login');
+        cy.get('[data-cy="email-input"]').type('bo@horvat.com', { delay: 100 });
+        cy.get('[data-cy="password-input"]').type('islanders123', { delay: 100 });
+        cy.get('[data-cy="login-form"]').submit();
+
+        cy.visit('http://localhost:8080/landlord-tenant-list', {delay: 100});
+        cy.get('[id="1.delete"]').click();
+
+        cy.visit('http://localhost:8080/landlord-tenant-list?', {delay: 100});
+        cy.get('[data-bs-target="#invite-modal"]').click({delay: 100});
+
+
+        cy.get('[id="email-input"]').type('joe@bob.com');
+        cy.get('[data-cy="submit-button"]').click()
+
+    })
+})
