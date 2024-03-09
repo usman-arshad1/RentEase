cd96efa9765f2bf8611b81e4ccb749bfa6647200
Usman Arshad <uarshad2012@gmail.com>
2024-03-08 23:10:05 -0800

    update to unit test

--- cypress/e2e/property.cy.js
+++ cypress/e2e/property.cy.js
@@ -5,12 +5,11 @@ describe('create property page', () => {
         cy.get('[data-cy="password-input"]').type('islanders123', { delay: 100 });
         cy.get('[data-cy="login-form"]').submit();
 
-        cy.visit('http://localhost:8080/landlord-properties?', {delay: 500});
         cy.get('[id="add-property').click({delay: 100})
 
         cy.get('[id="unit"]').type('1853', { delay: 100 });
-        cy.get('[id="street"]').type('Willingdon St', { delay: 100 });
-        cy.get('[id="city"]').type('Burnaby', { delay: 100 });
+        cy.get('[id="street"]').type('Trinidad St', { delay: 100 });
+        cy.get('[id="city"]').type('Tobago', { delay: 100 });
         cy.get('[id="province_state"]').type('BC', { delay: 100 });
         cy.get('[id="country"]').type('Canada', { delay: 100 });
         cy.get('[id="submit"]').click()
--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -6,10 +6,10 @@ describe('signup page', () => {
   })
 
   it('redirect to /login?message=success on success', () => {
-    cy.get('[data-cy="fname-input"]').type('Bo', { delay: 100 });
-    cy.get('[data-cy="lname-input"]').type('Horvat', { delay: 100 });
-    cy.get('[data-cy="email-input"]').type('bo@horvat.com', { delay: 100 });
-    cy.get('[data-cy="password-input"]').type('islanders123', { delay: 100 });
+    cy.get('[data-cy="fname-input"]').type('Cade', { delay: 100 });
+    cy.get('[data-cy="lname-input"]').type('Lee', { delay: 100 });
+    cy.get('[data-cy="email-input"]').type('cade@lee.com', { delay: 100 });
+    cy.get('[data-cy="password-input"]').type('cadelee123', { delay: 100 });
     cy.get('[data-cy="signup-form"]').submit();
     cy.url().should('include', '/login?message=success');
   })
