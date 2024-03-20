55107730d407c162a5daa026fb934a27aa623e21
syoo0295 <72421499+syoo0295@users.noreply.github.com>
2024-03-19 23:54:23 -0700

    Add dropdown lists for province/state and country selection

--- routes/property.js
+++ routes/property.js
@@ -24,94 +24,93 @@ function verifyLandlord(req, res, next) {
 
 
 router.get('/', verifyLandlord, async function(req, res, next) {
-	try {
+  try {
 	  const currentUserId = req.user.user_id;
 	  const userProperties = await prisma.properties.findMany({
-		where: {
+      where: {
 		  user_id: currentUserId,
-		},
+      },
 	  });
 	  res.render('property', {
-		title: 'Landlord Properties',
-		properties: userProperties,
-		userEmail: req.user.email,
+      title: 'Landlord Properties',
+      properties: userProperties,
+      userEmail: req.user.email,
 	  });
 	  console.log(req.user.email);
-	} catch (error) {
+  } catch (error) {
 	  console.error(error);
 	  res.status(500).send('An error occurred while fetching the properties');
-	}
-  });
+  }
+});
 
 // Delete property
-router.post("/:id", verifyLandlord, async (req, res) => {
-	const {id} =  req.params;
+router.post('/:id', verifyLandlord, async (req, res) => {
+  const {id} = req.params;
 
-	try {
-		const property = await prisma.properties.delete({
-			where:{
-				property_id:parseInt(id),
-			},
-		});
+  try {
+    const property = await prisma.properties.delete({
+      where: {
+        property_id: parseInt(id),
+      },
+    });
 
-		res.redirect("/landlord-properties")
-	} catch(e) {
-		res.status(500).send("Deletion failed")
-	}
-})
+    res.redirect('/landlord-properties');
+  } catch (e) {
+    res.status(500).send('Deletion failed');
+  }
+});
 
 // Update property page
-router.get("/update/:id", verifyLandlord, async (req, res) => {
-	const {id} = req.params;
-	const property_id = parseInt(id);
-	const user_id = req.user.user_id
+router.get('/update/:id', verifyLandlord, async (req, res) => {
+  const {id} = req.params;
+  const property_id = parseInt(id);
+  const user_id = req.user.user_id;
 
-	try {
-		const currentProperty = await prisma.properties.findUnique({
-			where: {
-				property_id: property_id,
-				user_id: user_id
-			}
-		})
-		if (!currentProperty) {
-			res.status(500).send("You can only access to your own property")
-		}
-		else {
-			res.render("update_property", {
-				title: "Update property",
-				property: currentProperty
-			})
-		}
-	} catch (error) {
-		res.status(500).send("Failed");
-	}
-})
+  try {
+    const currentProperty = await prisma.properties.findUnique({
+      where: {
+        property_id: property_id,
+        user_id: user_id,
+      },
+    });
+    if (!currentProperty) {
+      res.status(500).send('You can only access to your own property');
+    } else {
+      res.render('update_property', {
+        title: 'Update property',
+        property: currentProperty,
+      });
+    }
+  } catch (error) {
+    res.status(500).send('Failed');
+  }
+});
 // Update property
-router.post("/update/:id", verifyLandlord, async (req, res)=> {
-	const {id} = req.params
-	const {unit, street, city, province_state, country} = req.body
-	try {
-		const property_id = parseInt(id);
-		const property_unit = parseInt(unit);
+router.post('/update/:id', verifyLandlord, async (req, res)=> {
+  const {id} = req.params;
+  const {unit, street, city, province_state, country} = req.body;
+  try {
+    const property_id = parseInt(id);
+    const property_unit = parseInt(unit);
 
-		const updateProperty = await prisma.properties.update({
-			where: {
-				property_id: property_id
-			},
-			data:
+    const updateProperty = await prisma.properties.update({
+      where: {
+        property_id: property_id,
+      },
+      data:
 				{
-					unit:property_unit,
-					street: street,
-					city: city,
-					province_state: province_state,
-					country: country
+				  unit: property_unit,
+				  street: street,
+				  city: city,
+				  province_state: province_state,
+				  country: country,
 				},
-		})
-		res.redirect("/landlord-properties")
-	} catch(e) {
-		res.status(500).send("Update failed")
-	}
-})
+    });
+    res.redirect('/landlord-properties');
+  } catch (e) {
+    res.status(500).send('Update failed');
+  }
+});
 
 
 module.exports = router;
