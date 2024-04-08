1d9822a2234e83feb5d7ec2eec91fb6d0ab1c7dd
krispyrendang <taswinder.s@gmail.com>
2024-04-08 15:37:22 -0700

    fixed error on new_feedback.js

--- routes/new_feedback.js
+++ routes/new_feedback.js
@@ -79,7 +79,8 @@ async function newFeedback(req, res) {
 
     const curr_date = getDate();
     // console.log(curr_date); // "29-02-2024"
-    const unique_code = code.unique_code;
+    const unique_code = code.unique_code();
+    console.log("code: " + unique_code)
 
     await prisma.feedback.create({
       data: {
@@ -123,5 +124,88 @@ router.get('/', function(req, res, next) {
 
   res.render('new_feedback', {title: 'Tenant New Feedback', errorMsgs});
 });
+
+const base62_str = [
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z",
+];
+
+//Creates a random number between 1 million (4 char codeword) to 100 billion (7 char codeword), inclusive.
+function unique_code() {
+	let key = Math.floor(Math.random() * 1000000000) + 1000000;
+	let hash_str = base62_encode(key);
+
+	return hash_str;
+}
+
+//Takes a decimal number and returns a unique code for the shortURL
+function base62_encode(key) {
+	let hash_str = "";
+	while (key > 0) {
+		hash_str += base62_str[key % 62];
+		key = Math.floor((key /= 62));
+	}
+	return hash_str;
+}
 
 module.exports = router;
